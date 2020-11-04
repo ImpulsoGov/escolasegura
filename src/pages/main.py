695ccef9cc0c851f4d25a3a34f7c92bab6e5c09c
Dandara Sousa <dandaramcsousa@gmail.com>
2020-11-04 12:02:52 -0300

    tabela de referencias

--- src/pages/main.py
+++ src/pages/main.py
@@ -669,89 +669,82 @@ def  genReferencesContainer():
         f"""
         <div class="container main-padding">
             <div class="title-section">
-                <img class="square" src="https://i.imgur.com/gGIFS5N.png">Referências
+                <img class="square" src="https://i.imgur.com/gGIFS5N.png">Fontes e Referências
             </div>
-            <div class="text-title-section minor-padding"> Fontes para o Protocolo </div>
-            <div class="minor-padding">
-                <b>Título:</b> Orientações para Retomada Segura das Atividades Presenciais nas Escolas de Educação Básica no Contexto da Pandemia da COVID-19
-                <br>
-                <b>Fonte:</b> Ministério da Saúde
-                <br>
-                <b>URL:</b> <a href="http://antigo.saude.gov.br/images/pdf/2020/September/18/doc-orientador-para-retomada-segura-das-escolas-no-contexto-da-covid-19.pdf">
-                    http://antigo.saude.gov.br/images/pdf/2020/September/18/doc-orientador-para-retomada-segura-das-escolas-no-contexto-da-covid-19.pdf
-                </a>
-            </div>
-            <div class="minor-padding">
-                <b>Título:</b> Manual sobre Biossegurança para Reabertura de Escolas no Contexto da COVID-19
-                <br>
-                <b>Fonte:</b> Fiocruz
-                <br>
-                <b>URL:</b> <a href="https://portal.fiocruz.br/sites/portal.fiocruz.br/files/documentos/manualreabertura.pdf">
-                    https://portal.fiocruz.br/sites/portal.fiocruz.br/files/documentos/manualreabertura.pdf
-                </a>
-            </div>
-            <div class="minor-padding">
-                <b>Título:</b> Guia de Implementação de Protocolos de Retorno das Atividades Presenciais nas Escolas de Educação Básica
-                <br>
-                <b>Fonte:</b> Ministério da Educação
-                <br>
-                <b>URL:</b> <a href="https://www.gov.br/mec/pt-br/assuntos/GuiaderetornodasAtividadesPresenciaisnaEducaoBsica.pdf">
-                    https://www.gov.br/mec/pt-br/assuntos/GuiaderetornodasAtividadesPresenciaisnaEducaoBsica.pdf
-                </a>
-            </div>
-            <div class="minor-padding">
-                <b>Título:</b> Considerations for school-related public health measures in the context of COVID-19
-                <br>
-                <b>Fonte:</b> Organização Mundial da Saúde
-                <br>
-                <b>URL:</b> <a href="https://www.who.int/publications/i/item/considerations-for-school-related-public-health-measures-in-the-context-of-covid-19">
-                    https://www.who.int/publications/i/item/considerations-for-school-related-public-health-measures-in-the-context-of-covid-19
-                </a>
-            </div>
-            <div class="text-title-section minor-padding"> Fontes para os Materiais Necessários </div>
-            <div class="minor-padding">
-                <b>Título:</b> Manual de Protocolos de Saúde
-                <br>
-                <b>Fonte:</b> Governo do Estado do Amazonas
-                <br>
-                <b>URL:</b> <a href="http://www.educacao.am.gov.br/wp-content/uploads/2020/07/PROTOCOLOS-DE-SAuDE02.pdf">
-                    http://www.educacao.am.gov.br/wp-content/uploads/2020/07/PROTOCOLOS-DE-SAuDE02.pdf
-                </a>
-            </div>
-            <div class="minor-padding">
-                <b>Título:</b> Ferramenta de Planejamento e Cálculo de Custos de Preparações Alcoólicas para a Higiene das Mãos
-                <br>
-                <b>Fonte:</b> Proqualis
-                <br>
-                <b>URL:</b> <a href="https://proqualis.net/sites/proqualis.net/files/FerramentadePlanejamentoeClculodeCustosgrfica.pdf">
-                    https://proqualis.net/sites/proqualis.net/files/FerramentadePlanejamentoeClculodeCustosgrfica.pdf
-                </a>
-            </div>
-            <div class="minor-padding">
-                <b>Título:</b> Segurança do paciente em serviços de saúde: limpeza e desinfecção de superfícies
-                <b>Fonte:</b> ANVISA
-                <br>
-                <b>URL:</b> <a href="https://www20.anvisa.gov.br/segurancadopaciente/images/documentos/ManualLimpezaeDesinfeccaofinal.pdf">
-                    https://www20.anvisa.gov.br/segurancadopaciente/images/documentos/ManualLimpezaeDesinfeccaofinal.pdf
-                </a>
-            </div>
-            <div class="minor-padding">
-                <b>Título:</b> NOTA TÉCNICA Nº 47/2020/SEI/GIALI/GGFIS/DIRE4/ANVISA Uso de luvas e máscaras em estabelecimentos da área de alimentos no contexto do enfrentamento ao COVID-19
-                <br>
-                <b>Fonte:</b> ANVISA
-                <br>
-                <b>URL:</b> <a href="https://www.gov.br/anvisa/pt-br/arquivos-noticias-anvisa/310json-file-1">
-                    https://www.gov.br/anvisa/pt-br/arquivos-noticias-anvisa/310json-file-1
-                </a>
-            </div>
-            <div class="minor-padding">
-                <b>Título:</b> Appendix A – Risk-assessment for determining environmental cleaning method and frequency
-                <br>
-                <b>Fonte:</b> Centers for Disease Control and Prevention
-                <br>
-                <b>URL:</b> <a href="https://www.cdc.gov/hai/prevent/resource-limited/risk-assessment.html">
-                    https://www.cdc.gov/hai/prevent/resource-limited/risk-assessment.html
-                </a>
+            <div class="table-responsive">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Fonte</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> Orientações para Retomada Segura das Atividades Presenciais nas Escolas de Educação Básica no Contexto da Pandemia da COVID-19 </td>
+                        <td> <a href="http://antigo.saude.gov.br/images/pdf/2020/September/18/doc-orientador-para-retomada-segura-das-escolas-no-contexto-da-covid-19.pdf">
+                            Ministério da Saúde
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Manual sobre Biossegurança para Reabertura de Escolas no Contexto da COVID-19 </td>
+                        <td> <a href="https://portal.fiocruz.br/sites/portal.fiocruz.br/files/documentos/manualreabertura.pdf">
+                            Fiocruz
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Guia de Implementação de Protocolos de Retorno das Atividades Presenciais nas Escolas de Educação Básica </td>
+                        <td> <a href="https://www.gov.br/mec/pt-br/assuntos/GuiaderetornodasAtividadesPresenciaisnaEducaoBsica.pdf">
+                            Ministério da Educação
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Considerations for school-related public health measures in the context of COVID-19 </td>
+                        <td> <a href="https://www.who.int/publications/i/item/considerations-for-school-related-public-health-measures-in-the-context-of-covid-19">
+                            Organização Mundial da Saúde
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Manual de Protocolos de Saúde </td>
+                        <td> <a href="http://www.educacao.am.gov.br/wp-content/uploads/2020/07/PROTOCOLOS-DE-SAuDE02.pdf">
+                            Governo do Estado do Amazonas
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Ferramenta de Planejamento e Cálculo de Custos de Preparações Alcoólicas para a Higiene das Mãos </td>
+                        <td> <a href="https://proqualis.net/sites/proqualis.net/files/FerramentadePlanejamentoeClculodeCustosgrfica.pdf">
+                            Proqualis
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Segurança do paciente em serviços de saúde: limpeza e desinfecção de superfícies </td>
+                        <td> <a href="https://www20.anvisa.gov.br/segurancadopaciente/images/documentos/ManualLimpezaeDesinfeccaofinal.pdf">
+                            ANVISA
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> NOTA TÉCNICA Nº 47/2020/SEI/GIALI/GGFIS/DIRE4/ANVISA <br> Uso de luvas e máscaras em estabelecimentos da área de alimentos no contexto do enfrentamento ao COVID-19 </td>
+                        <td> <a href="https://www.gov.br/anvisa/pt-br/arquivos-noticias-anvisa/310json-file-1">
+                            ANVISA
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Appendix A – Risk-assessment for determining environmental cleaning method and frequency </td>
+                        <td> <a href="https://www.cdc.gov/hai/prevent/resource-limited/risk-assessment.html">
+                            Centers for Disease Control and Prevention
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
         </div>
         """,
