1c0a992a921ec2fbd807028108079bc0624dc338
Gabrielle Arruda <gabriellearrudac@gmail.com>
2020-11-10 16:45:20 -0300

    Update simulationContainer.py

--- src/pages/simulationContainer.py
+++ src/pages/simulationContainer.py
@@ -9,57 +9,6 @@ from ipywidgets import AppLayout, GridspecLayout
 
 from model.get_school_return_data import entrypoint
 
-def read_data(country, config, endpoint):
-
-    if os.getenv("IS_LOCAL") == "TRUE":
-        api_url = config[country]["api"]["local"]
-    else:
-        api_url = config[country]["api"]["external"]
-
-    url = api_url + endpoint
-
-    print("\nLoad data from:", url)
-    df = pd.read_csv(url)
-    return df
-
-
-@st.cache(suppress_st_warning=True)
-def get_data(config):
-    df = read_data("br", config, "br/cities/safeschools/main")
-    return df
-
-def genHeroSection(title1: str, title2: str, subtitle: str, header: bool):
-
-    if header:
-        header = """<a href="https://coronacidades.org/" target="blank" class="logo-link"><span class="logo-header" style="font-weight:bold;">corona</span><span class="logo-header" style="font-weight:lighter;">cidades</span> <br></a>"""
-    else:
-        header = """<br>"""
-
-    st.write(
-        f"""
-        <div class="container">
-            {header}
-        <div class="grid-container-header">
-            <div class="div1-head">
-                <img class="img-logo-header" src="https://i.imgur.com/SJHwG4Z.png">
-            </div>
-            <div class="div2-head">
-                <span class="hero-container-product main-blue-span">{title1}</span>
-                <br>
-                <span class="hero-container-product main-blue-span">{title2}</span>
-                <br><br>
-            </div>
-            <div class="div3-head">
-                <span class="hero-container-question">
-                Controle a Covid-19 e promova aulas presenciais seguras na rede pública.
-                </span>
-            </div>
-            <br>
-        </div>
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
 
 def genSimulationResult(params, config):
 
@@ -174,71 +123,12 @@ def genSimulationResult(params, config):
         unsafe_allow_html=True,
     )
 
-def genSelectBox(df, session_state):
+def genSimulationContainer(df, config, session_state):
     st.write(
         f"""
-        <div class="container main-padding">
-            <div class="text-title-section"> Selecione sua rede </div>
+        <div class="title-section">
+            <p style="color:#2b14ff; font-size:21px;"><b>4 - Qual é o modelo de retorno híbrido mais adequado para mim e qual a melhor logística e materiais necessários para isso?</b></p>
         </div>
-        """,
-        unsafe_allow_html=True,
-    )
-    col1, col2, col3, col4 = st.beta_columns([0.3, 0.5, 0.5, 1])
-
-    with col1:
-        session_state.state_id = st.selectbox("Estado", utils.filter_place(df, "state"))
-    with col2:
-        options_city_name = utils.filter_place(
-            df, "city", state_id=session_state.state_id
-        )
-        options_city_name = pd.DataFrame(data=options_city_name, columns=["city_name"])
-        x = int(
-            options_city_name[options_city_name["city_name"] == "Todos"].index.tolist()[
-                0
-            ]
-        )
-        session_state.city_name = st.selectbox("Município", options_city_name, index=x)
-    with col3:
-        options_adiminlevel = utils.filter_place(
-            df,
-            "administrative_level",
-            state_id=session_state.state_id,
-            city_name=session_state.city_name,
-        )
-        options_adiminlevel = pd.DataFrame(
-            data=options_adiminlevel, columns=["adiminlevel"]
-        )
-        y = int(
-            options_adiminlevel[
-                options_adiminlevel["adiminlevel"] == "Todos"
-            ].index.tolist()[0]
-        )
-        session_state.administrative_level = st.selectbox(
-            "Nível de Administração", options_adiminlevel, index=y
-        )
-    with col4:
-        st.write(
-            f"""
-        <div class="container main-padding">
-            <br>
-        </div>
-        """,
-            unsafe_allow_html=True,
-        )
-
-def genSimulationContainer(session_state):
-    utils.localCSS("style.css")
-    genHeroSection(
-        title1="Escola",
-        title2="Segura",
-        subtitle="{descrição}",
-        header=True,
-    )
-    config = yaml.load(open("config/config.yaml", "r"), Loader=yaml.FullLoader)
-    df = get_data(config)
-    genSelectBox(df, session_state)
-    st.write(
-        f"""
         <div class="container main-padding">
             <div class="text-title-section"> Simule o retorno </div>
                 <div >
@@ -320,13 +210,13 @@ def genSimulationContainer(session_state):
         unsafe_allow_html=True,
     )
     if "Rural" in data["school_location"].drop_duplicates().values:
-        rural = ["Rural" if st.checkbox("Quero considerar apenas escolas rurais") else "Todos"][0]
+        rural = ["Rural" if st.checkbox("Apenas escolas rurais") else "Todos"][0]
 
         data = data[(data["school_location"] == rural)]
 
     if "Sim" in data["school_public_water_supply"].drop_duplicates().values:
         water_supply = [
-            "Sim" if st.checkbox("Quero considerar apenas escolas com água encanada") else "Todos"
+            "Sim" if st.checkbox("Apenas escolas com água encanada") else "Todos"
         ][0]
 
         data = data[(data["school_public_water_supply"] == water_supply)]
