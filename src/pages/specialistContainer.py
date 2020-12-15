c48611672dd63a783086e6eda6e1d427e85650a4
Dandara Sousa <dandaramcsousa@gmail.com>
2020-12-15 10:27:35 -0300

    modifica mensagem do banner

--- src/pages/specialistContainer.py
+++ src/pages/specialistContainer.py
@@ -6,7 +6,7 @@ def genSpecialistContainer():
     This is a function that returns the "Specialist" session
               
     """
-    st.write(
+    '''st.write(
         f"""
         <div class="container">
                 <div class="left-margin card-plan">
@@ -21,6 +21,32 @@ def genSpecialistContainer():
                                 <div class="main-padding" align="center" style="padding-bottom: 10px;">
                                     <a href="https://forms.gle/MrkuQ9H4WwEYjbw98" target=_blank>
                                     <button class="button"; style="border-radius: .25rem;">inscreva-se ></button><br>
+                                    </a><br>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )'''
+    st.write(
+        f"""
+        <div class="container">
+                <div class="left-margin card-plan">
+                        <div style="margin:10px">
+                            <div class="left-margin">
+                                <div class="text-title-section minor-padding"> 
+                                Encontro com o <span class="bold main-orange-span">especialista </span></div>
+                                <div class="minor-padding main-black-span"><br>
+                                Realizamos, semanalmente, um encontro online com <b>Wanderson de Oliveira</b>, 
+                                epidemiologista e ex-secretário nacional de vigilância em saúde, para solucionar dúvidas da 
+                                gestão pública sobre o processo de retomada de atividades presenciais na rede de ensino. 
+                                Estamos em recesso até o dia xx de janeiro, mas você pode ver o material produzido até agora 
+                                durante os encontros. 
+                                <div class="main-padding" align="center" style="padding-bottom: 10px;">
+                                    <a href="https://forms.gle/MrkuQ9H4WwEYjbw98" target=_blank>
+                                    <button class="button"; style="border-radius: .25rem;">conheça as dúvidas ></button><br>
                                     </a><br>
                                 </div>
                             </div>
