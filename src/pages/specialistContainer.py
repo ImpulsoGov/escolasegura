c6f1a820e4439a701e926f50fa1b8161f93a7c84
Dandara Sousa <dandaramcsousa@gmail.com>
2020-12-17 10:34:35 -0300

    upade banner

--- src/pages/specialistContainer.py
+++ src/pages/specialistContainer.py
@@ -39,14 +39,13 @@ def genSpecialistContainer():
                                 <div class="text-title-section minor-padding"> 
                                 Encontro com o <span class="bold main-orange-span">especialista </span></div>
                                 <div class="minor-padding main-black-span"><br>
-                                Realizamos, semanalmente, um encontro online com <b>Wanderson de Oliveira</b>, 
-                                epidemiologista e ex-secretário nacional de vigilância em saúde, para solucionar dúvidas da 
-                                gestão pública sobre o processo de retomada de atividades presenciais na rede de ensino. 
-                                Estamos em recesso até o dia xx de janeiro, mas você pode ver o material produzido até agora 
-                                durante os encontros. 
+                                Em dezembro, realizamos uma série de encontros com <b>Wanderson Oliveira</b>, 
+                                ex-secretário nacional de vigilância em saúde, para falar sobre uma reabertura planejada da 
+                                rede pública de ensino. 
+                                Você pode conferir um resumo das discussões e tirar suas dúvidas sobre o assunto.
                                 <div class="main-padding" align="center" style="padding-bottom: 10px;">
-                                    <a href="https://forms.gle/MrkuQ9H4WwEYjbw98" target=_blank>
-                                    <button class="button"; style="border-radius: .25rem;">conheça as dúvidas ></button><br>
+                                    <a href="https://coronacidades.org/encontro-online-da-escola-segura-discute-reabertura-responsavel-e-gradual-da-rede-publica-diante-da-covid/" target=_blank>
+                                    <button class="button"; style="border-radius: .25rem;">tire suas dúvidas ></button><br>
                                     </a><br>
                                 </div>
                             </div>
