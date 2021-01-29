39dbbbdb49bbabd9e65e3b6e900ac7e2b29768e2
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-01-29 11:18:16 -0300

    Update main.py

--- src/pages/main.py
+++ src/pages/main.py
@@ -196,17 +196,17 @@ def main(session_state):
         "opened escolasegura", session_state, is_new_page=True
     )
 
-    # TEMPORARY ALERT BANNER
-    st.write(
-        """
-        <div class="col card-simulator" style="background:#ff8f8f; border-radius:30px;">
-            <div class="row" style="font-family: 'Roboto Condensed', sans-serif; margin-bottom:0px; padding:10px; font-size:21px;">
-            <b>IMPORTANTE: Esse é um site que usamos para testagem, se você está 
-            procurando nosso site oficial do escola segura clique <a href="https://escolasegura.coronacidades.org/">AQUI</a> ou acesse o link: https://escolasegura.coronacidades.org/.</b>
-            </div>
-        </div>""",
-        unsafe_allow_html=True,
-    )
+#     # TEMPORARY ALERT BANNER
+#     st.write(
+#         """
+#         <div class="col card-simulator" style="background:#ff8f8f; border-radius:30px;">
+#             <div class="row" style="font-family: 'Roboto Condensed', sans-serif; margin-bottom:0px; padding:10px; font-size:21px;">
+#             <b>IMPORTANTE: Esse é um site que usamos para testagem, se você está 
+#             procurando nosso site oficial do escola segura clique <a href="https://escolasegura.coronacidades.org/">AQUI</a> ou acesse o link: https://escolasegura.coronacidades.org/.</b>
+#             </div>
+#         </div>""",
+#         unsafe_allow_html=True,
+#     )
 
 
     utils.localCSS("style.css")
