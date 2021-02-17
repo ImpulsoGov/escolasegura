b547954e8f573f8590ca9ffd76b26e902f83c082
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-02-17 11:44:35 -0300

    Update specialistContainer.py

--- src/pages/specialistContainer.py
+++ src/pages/specialistContainer.py
@@ -33,26 +33,26 @@ def genSpecialistContainer():
     st.write(
         f"""
         <div class="conteudo upper-padding">
-                <div class="card-plan">
-                        <div style="margin:10px">
-                            <div>
-                                <div style="font-size:1.5em;"> 
-                                    Encontro com o <span style="color:#ff9147;"><b>especialista</b></span>
-                                </div>
-                                <div><br>
-                                    Em dezembro, realizamos uma série de encontros com <b>Wanderson Oliveira</b>, 
-                                    ex-secretário nacional de vigilância em saúde, para falar sobre uma reabertura planejada da 
-                                    rede pública de ensino. 
-                                    Você pode conferir um resumo das discussões e tirar suas dúvidas sobre o assunto.
-                                    <div align="center" style="padding-bottom: 10px;">
-                                        <a href="https://coronacidades.org/encontro-online-da-escola-segura-discute-reabertura-responsavel-e-gradual-da-rede-publica-diante-da-covid/" target=_blank>
-                                        <button class="button"; style="border-radius: .25rem;">tire suas dúvidas ></button><br>
-                                        </a><br>
-                                    </div>
-                                </div>
+            <div class="card-plan" style="width:100%;">
+                <div style="margin:10px">
+                    <div>
+                        <div style="word-break: break-word; font-size:1.5em;">
+                        Encontro com o <span style="color:#ff9147;"><b>especialista</b></span>
+                        </div>
+                        <div><br>
+                            Em dezembro, realizamos uma série de encontros com <b>Wanderson Oliveira</b>, 
+                            ex-secretário nacional de vigilância em saúde, para falar sobre uma reabertura planejada da 
+                            rede pública de ensino. 
+                            Você pode conferir um resumo das discussões e tirar suas dúvidas sobre o assunto.
+                            <div align="center" style="padding-bottom: 10px;">
+                                <a href="https://coronacidades.org/encontro-online-da-escola-segura-discute-reabertura-responsavel-e-gradual-da-rede-publica-diante-da-covid/" target=_blank>
+                                <button class="button"; style="border-radius: .25rem;">tire suas dúvidas ></button><br>
+                                </a><br>
                             </div>
                         </div>
+                    </div>
                 </div>
+            </div>
         </div>
         """,
         unsafe_allow_html=True,
