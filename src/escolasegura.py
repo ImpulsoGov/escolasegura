26d93b06540c257c947023ad035df09562b4af79
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-02-16 23:31:56 -0300

    Update escolasegura.py

--- src/escolasegura.py
+++ src/escolasegura.py
@@ -3,17 +3,19 @@ import streamlit as st
 import yaml
 import session
 import time
-
 import pages.about as sobre
 import pages.inicio as inicio
 import pages.passos as passos
 import pages.simulationContainer as simulation
 import pages.simulacao as simulacao
 import pages.referencesContainer as referencias
-
+import socket
 import utils
 import pages.simulationContainer as sc
 from PIL import Image
+import pathlib
+from flask import Flask, request
+from flask.wrappers import Request
 
 
 st.set_page_config(
@@ -21,6 +23,62 @@ st.set_page_config(
     page_icon=Image.open("imgs/escolasegura_favicon.png"),
     layout='wide',
     initial_sidebar_state='collapsed')
+
+# PAGES = {   
+#     "Inicio" : inicio,
+#     "Guia 10 passos" : passos,
+#     "Simulacão" : simulation,
+#     "Simulacao" : simulacao,
+#     "Quem somos?" : sobre,
+#     "Fontes e Referências": referencias
+# }
+
+# page = st.sidebar.radio(
+#     "Menu", list(PAGES.keys()),
+# )
+
+
+# # Remove menu da visualizacao
+# hide_streamlit_style = """
+#     <style>
+#     #MainMenu {visibility: hidden;}
+#     footer {visibility: hidden;}
+#     </style>
+
+#     """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# # SESSION STATE
+# time.sleep(
+#     0.05
+# )  # minimal wait time so we give time for the user session to appear in steamlit
+# session_state = session.SessionState.get(
+#     key=session.get_user_id(),
+#     update=False,
+#     state_name="Acre",
+#     state_id="AC",
+#     city_name="Todos",
+#     administrative_level="Todos",
+#     refresh=False,
+#     reset=False,
+#     already_generated_user_id=None,
+#     pages_open=None,
+#     amplitude_events=None,
+#     button_styles=dict(),
+#     continuation_selection=None,
+#     button_simule=0,
+#     section1_organize=False,
+#     section2_manage=False
+# )
+# utils.applyButtonStyles(session_state)
+# PAGES[page].main(session_state)
+# # query_params = st.experimental_get_query_params()
+# # page_param = query_params.get("page", [0])
+# # if query_params:
+# #     PAGES[page_param[0]].main(session_state)
+# # else:
+# #     PAGES[page].main(session_state)
+
 
 
 def main():
@@ -34,7 +92,6 @@ def main():
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
         </style>
-
         """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
@@ -61,7 +118,7 @@ def main():
         section2_manage=False,
     )
 
-    PAGES = {	
+    PAGES = {   
         "Inicio" : inicio,
         "Guia 10 passos" : passos,
         "Simulation" : simulation,
