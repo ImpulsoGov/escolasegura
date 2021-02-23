c38cf8a4fb0412adf266f3db8b8215df4270438d
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-02-23 10:21:23 -0300

    Update about.py

--- src/pages/about.py
+++ src/pages/about.py
@@ -14,40 +14,53 @@ def main():
     utils.localCSS("localCSS.css")
     he.genHeader("sobre")
     utils.main_title(title="<b>Quem Somos?</b>", subtitle="")
-    # impulsodescricao = """A Impulso é uma organização não governamental com a missão auxiliar governos na melhora da entrega de 
-    # serviços públicos de saúde à população através do uso de dados e tecnologia, apoiando o processo de 
-    # tomada de decisão e visando o aprimoramento contínuo de políticas públicas. Foi fundada em 2019 e é 
-    # uma das idealizadoras da plataforma <a target="_blank" href="https://coronacidades.org/">CoronaCidades.org</a>."""
-    # biddescricao = """"O Banco Interamericano de Desenvolvimento tem como missão melhorar vidas. 
-    # Criado em 1959, o BID é uma das principais fontes de financiamento de longo prazo para o desenvolvimento 
-    # econômico, social e institucional da América Latina e do Caribe. O BID também realiza projetos de pesquisas 
-    # de vanguarda e oferece assessoria sobre políticas, assistência técnica e capacitação a clientes públicos e 
-    # privados em toda a região."""
-    # lemanndescricao = """"A Fundação Lemann acredita que um Brasil feito por todos e para todos é um Brasil 
-    # que acredita no seu maior potencial: gente. Isso só acontece com educação de qualidade e com 
-    # o apoio a pessoas que querem resolver os grandes desafios sociais do país. Nós realizamos 
-    # projetos ao lado de professores, gestores escolares, secretarias de educação e governos 
-    # por uma aprendizagem de qualidade. Também apoiamos centenas de talentos, lideranças e organizações 
-    # que trabalham pela transformação social. Tudo para ajudar a construir um país mais justo, 
-    # inclusivo e avançado. Saiba mais em: <a target="_blank" href="fundacaolemann.org.br">fundacaolemann.org.br</a>"""
-    # imaninabledescricao = """"Imaginable Futures é uma empresa de investimento filantrópico global que acredita 
-    # que a aprendizagem tem o poder de estimular o potencial humano e tem como missão oferecer 
-    # a cada aluno oportunidades e ferramentas para que eles imaginem e realizem um futuro brilhante. 
-    # Com compromisso com a parceria e a cocriação, a organização está capacitando alunos, famílias 
-    # e comunidades para serem os agentes que moldam o futuro. A Imaginable Futures é um empreendimento 
-    # do The Omidyar Group, fundada e financiada por Pierre e Pam Omidyar."""
-    # formardescricao = """"O programa Formar foi concebido na Fundação Lemann e atua em parceria com 
-    # redes públicas de educação em todo o Brasil. Sua gestão é feita por uma equipe 
-    # multidisciplinar de consultores e especialistas que buscam o aprimoramento da gestão 
-    # pedagógica e administrativa, a partir do engajamento de dirigentes e equipes gestoras 
-    # das secretarias e escolas que compõem os sistemas de educação. Buscam também estimular 
-    # a adoção de políticas públicas perenes que contribuam na melhoria do processo de 
-    # aprendizagem juntamente com professores e estudantes."""
-    # utils.gen_title(title="Impulso", subtitle=impulsodescricao)
-    # utils.gen_title(title="Banco Interamericano de Desenvolvimento", subtitle=biddescricao)
-    # utils.gen_title(title="Fundação Lemann", subtitle=lemanndescricao)
-    # utils.gen_title(title="Imaginable Futures", subtitle=imaninabledescricao)
-    # utils.gen_title(title="Programa Formar", subtitle=formardescricao)
+    utils.gen_title(title="Sobre Nós", subtitle="")
+    impulsodescricao = """A Impulso é uma organização não governamental com a missão auxiliar governos na melhora da entrega de 
+    serviços públicos de saúde à população através do uso de dados e tecnologia, apoiando o processo de 
+    tomada de decisão e visando o aprimoramento contínuo de políticas públicas. Foi fundada em 2019 e é 
+    uma das idealizadoras da plataforma <a target="_blank" href="https://coronacidades.org/">CoronaCidades.org</a>."""
+    biddescricao = """"O Banco Interamericano de Desenvolvimento tem como missão melhorar vidas. 
+    Criado em 1959, o BID é uma das principais fontes de financiamento de longo prazo para o desenvolvimento 
+    econômico, social e institucional da América Latina e do Caribe. O BID também realiza projetos de pesquisas 
+    de vanguarda e oferece assessoria sobre políticas, assistência técnica e capacitação a clientes públicos e 
+    privados em toda a região."""
+    lemanndescricao = """"A Fundação Lemann acredita que um Brasil feito por todos e para todos é um Brasil 
+    que acredita no seu maior potencial: gente. Isso só acontece com educação de qualidade e com 
+    o apoio a pessoas que querem resolver os grandes desafios sociais do país. Nós realizamos 
+    projetos ao lado de professores, gestores escolares, secretarias de educação e governos 
+    por uma aprendizagem de qualidade. Também apoiamos centenas de talentos, lideranças e organizações 
+    que trabalham pela transformação social. Tudo para ajudar a construir um país mais justo, 
+    inclusivo e avançado. Saiba mais em: <a target="_blank" href="fundacaolemann.org.br">fundacaolemann.org.br</a>"""
+    imaninabledescricao = """"Imaginable Futures é uma empresa de investimento filantrópico global que acredita 
+    que a aprendizagem tem o poder de estimular o potencial humano e tem como missão oferecer 
+    a cada aluno oportunidades e ferramentas para que eles imaginem e realizem um futuro brilhante. 
+    Com compromisso com a parceria e a cocriação, a organização está capacitando alunos, famílias 
+    e comunidades para serem os agentes que moldam o futuro. A Imaginable Futures é um empreendimento 
+    do The Omidyar Group, fundada e financiada por Pierre e Pam Omidyar."""
+    formardescricao = """"O programa Formar foi concebido na Fundação Lemann e atua em parceria com 
+    redes públicas de educação em todo o Brasil. Sua gestão é feita por uma equipe 
+    multidisciplinar de consultores e especialistas que buscam o aprimoramento da gestão 
+    pedagógica e administrativa, a partir do engajamento de dirigentes e equipes gestoras 
+    das secretarias e escolas que compõem os sistemas de educação. Buscam também estimular 
+    a adoção de políticas públicas perenes que contribuam na melhoria do processo de 
+    aprendizagem juntamente com professores e estudantes."""
+    st.write(
+        f"""
+        <div class="conteudo" style="padding-bottom: 10px;">
+            <b>Impulso</b><br>{impulsodescricao}<br>
+        </div>
+        <div class="conteudo" style="padding-bottom: 10px;">
+            <b>Fundação Lemann</b><br>{lemanndescricao}<br>
+        </div>
+        <div class="conteudo" style="padding-bottom: 10px;">
+            <b>Imaginable Futures</b><br>{imaninabledescricao}<br>
+        </div>
+        <div class="conteudo" style="padding-bottom: 10px;">
+            <b>Programa Formar</b><br>{formardescricao}<br>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
     embasamento = """Para a construção do Escola Segura, revisamos a literatura científica, consultamos protocolos encontrados em manuais de retomada do ensino presencial tanto no Brasil como no exterior e pesquisamos as melhores práticas e seus resultados para trazer informações atualizadas e embasadas na ciência. Não menos importante, conversamos com gestores escolares das diversas regiões do país para compreender os desafios enfrentados por eles em seu trabalho com a rede escolar e com a comunidade. Essas conversas levantaram questões e desafios que guiaram nossa busca pelo o que há de mais consolidado na literatura científica e nas diretrizes técnicas de órgãos reguladores sobre uma retomada segura das atividades em ambiente escolar.<br><br>
 Após a revisão de documentos técnicos e guias operacionais realizados por agências especializadas como o Center of Disease Control and Prevention (CDC - EUA)[15][20],pela  Escola de Saúde Pública T.H. Chan da Universidade de Harvard[21], pela Organização Mundial da Saúde (OMS)[19], pelo MInistério da Educação[18] e da Saúde[16] do Brasil, pela Fiocruz[17] e pelo Município de Sobral[22] e realizando conversas técnicas com especialistas na área de Educação e gestão escolar, desenvolvemos os 10 Passos, uma ferramenta para guiar a retomada presencial das aulas nas escolas. O objetivo dos 10 Passos é  fornecer ao gestor uma visão objetiva sobre as principais macro ações a serem tomadas durante o processo de retomada das atividades presenciais nas escolas, servindo de guia para reflexões mais aprofundadas sobre cada etapa em um segundo momento.<br><br>
 Para esse segundo momento, o Escola Segura traz uma série de ferramentas auxiliares como o Simulador de Retomada, Plano de Ação e Monitoramento, Ferramentas de Verificação e Notificação de Casos Suspeitos e/ou Confirmados, e Protocolos de Atuação.<br><br>
