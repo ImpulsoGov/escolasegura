a670a20cdfe009b681ed0677dab1bfa05fe17c70
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-02-23 15:07:55 -0300

    Update header.py

--- src/pages/header.py
+++ src/pages/header.py
@@ -10,8 +10,8 @@ def genHeader(active):
 	escola = utils.load_image("imgs/escolasegura.png")
 	header = utils.load_image("imgs/grey_header.png")
 	# urlpath = 'http://localhost:8501/'
-	# urlpath = 'https://escolasegura-staging.herokuapp.com/'
-	urlpath = 'https://escolasegura.coronacidades.org/'
+	urlpath = 'https://escolasegura-staging.herokuapp.com/'
+	# urlpath = 'https://escolasegura.coronacidades.org/'
 	# import pdb; pdb.set_trace()
 	if active=="guia10passos":
 		st.write(
