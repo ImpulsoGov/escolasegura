c658aec0110c28cfdd202a23b97e4199a118398c
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-03-03 11:52:58 -0300

    Update passos.py

--- src/pages/passos.py
+++ src/pages/passos.py
@@ -235,7 +235,7 @@ Ação 2. Acompanhe também a situação epidemiológica da rede e dos casos nas
                                                 A ferramenta fornece também rotinas a serem seguidas dentro e fora da sala de aula.<br><br>
                                             </div><br>
                                             <div class="button-position" style="padding-bottom: 10px;">
-                                                <a href="https://docs.google.com/spreadsheets/d/1_fYMo_Cy3ukJCmxdKDa9plTOJDBfbURQSY2z0wGCyTY/view" target="blank_">
+                                                <a href="https://drive.google.com/file/d/1T8OsZ5PKFC5vn6QrtSJdkOlONaJjS8lc/view?usp=sharing" target="blank_">
                                                 <button class="button"; style="border-radius: .25rem;">acesse ></button><br>
                                                 </a>
                                             </div>
